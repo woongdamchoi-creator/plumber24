15fdfa1ef8d678d6b07212c8b9b871e2f9bf3b09
woongdamchoi-creator <woongdamchoi@gmail.com>
2026-05-24 08:07:06 +0900

    fix: sitemap integration 추가 (@astrojs/sitemap)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://plumbers24.netlify.app',
   integrations: [
     tailwind(),
+    sitemap(),
   ],
   output: 'static',
   build: {
